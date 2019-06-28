3dd3e017f90e98179eb9ae1d661ce6e02c89f041
ManPetrauskas <m.petrauskas98@gmail.com>
2019-06-28 16:24:59 +0300

    sql updates

--- app/src/main/java/com/example/supersaugykla/Main.java
+++ app/src/main/java/com/example/supersaugykla/Main.java
@@ -12,15 +12,17 @@ public class Main {
         ConnectionClass connectionClass = new ConnectionClass();
         Connection connection = connectionClass.getConnection();
 
-        String sql = "INSERT INTO workers (`First Name`,`Last Name`,`Login Token`,`Total hours`, `Last time ended`)" +
-                " VALUES ('Alio','Kelmas','belessskas16','0','NULL');";
+        String sql = "INSERT INTO workers (`First Name`,`Last Name`,`Login Token`) VALUES ('Vardenis','Pavardenis','gvdsfgbxcg41');";
 
-        String sqlUpdate = "UPDATE workers SET `Last time ended` = CURRENT_TIMESTAMP WHERE `ID`=13;";
+        String sqlUpdateEndWork = "UPDATE workers SET `Last time ended` = CURRENT_TIMESTAMP WHERE `ID`=15;";
+
+        String sqlUpdateHours = "UPDATE workers SET `Total hours` = TIMESTAMPDIFF(HOUR,`Last time started`,`Last time ended`);";
 
 
         Statement statement = connection.createStatement();
-        statement.executeUpdate(sql);                       //Naujas useris
-        statement.executeUpdate(sqlUpdate);                 //Updatina userio darbo baigima
+        //statement.executeUpdate(sql);                               //Naujas useris
+        //statement.executeUpdate(sqlUpdateEndWork);                  //Updatina userio darbo baigima
+        statement.executeUpdate(sqlUpdateHours);                    //Updatina visu useriu dienos valandas suskaiciuotas (reik kaskart updatint kad atsinaujintu)
         System.out.println("data inserted");
         connection.close();
     }
